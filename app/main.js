f8058606c5a7de57713aa71fa5b1aea0c2f88c2d
bzr-lipe <bzr.lipe@gmai.com>
2023-06-03 12:52:51 -0300

    desconto aplicado - usando map

--- app/main.js
+++ app/main.js
@@ -1,30 +1,14 @@
 let livros = [];
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaApi();
-const containerLivros = document.getElementById('livros')
 // async significa que essa função só rodará quando ela receber a promessa (pois poderia acontecer de demorar muito para receber os dados da api, então para não deixar o codigo lento, usamos o async)
 
 async function getBuscarLivrosDaApi() {
     const res = await fetch(endpointDaAPI);
     livros = await res.json();
-    exibirLivros(livros)
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirLivros(livrosComDesconto)
+    console.log(livrosComDesconto)
 }
 
-function exibirLivros (listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        let preco = livro.preco.toFixed(2)
-        containerLivros.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo">
-          ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$${preco}</p>
-        <div class="tags">
-          <span class="tag">${livro.categoria}</span>
-        </div>
-      </div>
-        `
-    })
-}
+
